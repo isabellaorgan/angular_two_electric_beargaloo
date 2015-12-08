29ef8ac0b3fe04c75ccf9d0463e5b357c716267a
Isabella Organ <isabellaorgan@gmail.com>
2015-12-08 09:47:21 -0800

    sassed bears

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var minifyCss = require('gulp-minify-css');
-var concatCss = require('gulp-concat-css');
 var gulpWatch = require('gulp-watch');
 var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
@@ -11,17 +10,17 @@ gulp.task('static:dev', function() {
   .pipe(gulp.dest('build/'));
 });
 
-gulp.task('css:dev', function() {
-	return gulp.src('./app/sass/**/*.scss')
-	.pipe(maps.init())
-	.pipe(sass().on('error', sass.logError))
-	.pipe(minifyCss())
-	.pipe(maps.write('/'))
-	.pipe(gulp.dest('build/'));
+gulp.task('sass:dev', function() {
+  gulp.src('./app/sass/**/*.scss')
+  .pipe(maps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCss())
+  .pipe(maps.write('./'))
+  .pipe(gulp.dest('build/css/'));
 });
 
-gulp.task('css:watch', function () {
-  gulp.watch('./app/css/**/*.css', ['css:dev']);
+gulp.task('sass:watch', function () {
+  gulp.watch(['./app/sass/**/*.scss', './app/index.html'], ['sass:dev', 'static:dev']);
 });
 
 gulp.task('webpack:dev', function() {
@@ -44,5 +43,5 @@ gulp.task('webpack:test', function() {
 	.pipe(gulp.dest('test/client/'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'static:dev', 'css:dev']);
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'sass:dev']);
 gulp.task('default', ['build:dev']);
